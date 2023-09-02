a0c9698ff329529111e317edb9245b1b6e772fa3
KS Foong <khangfoong@gmail.com>
2023-09-02 14:18:38 +0800

    Add custom theme and font that support chinese
    Signed-off-by: KS Foong <khangfoong@gmail.com>

---
+++ theme/theme.go
@@ -0,0 +1,28 @@
+package theme
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
+)
+
+type MyTheme struct{}
+
+var asd fyne.Theme = (*MyTheme)(nil)
+
+func (*MyTheme) Font(s fyne.TextStyle) fyne.Resource {
+	return resourceNotoSansSCLightTtf
+}
+
+func (*MyTheme) Color(n fyne.ThemeColorName, v fyne.ThemeVariant) color.Color {
+	return theme.DefaultTheme().Color(n, v)
+}
+
+func (*MyTheme) Icon(n fyne.ThemeIconName) fyne.Resource {
+	return theme.DefaultTheme().Icon(n)
+}
+
+func (*MyTheme) Size(n fyne.ThemeSizeName) float32 {
+	return theme.DefaultTheme().Size(n)
+}
